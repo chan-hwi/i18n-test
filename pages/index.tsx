592b17ea2bed5647353ce35fffaa9011a7fbcadb
Chanhwi Hwang <gazebo5@naver.com>
2023-06-05 17:19:37 +0900

    fix: type issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { UseTranslation, useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { GetStaticProps } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,11 +11,13 @@ export default function Home() {
 	return <>hello world {t("이름")}</>;
 }
 
-export async function getStaticProps({ locale }) {
+type Props = {};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => {
 	return {
 		props: {
-			...(await serverSideTranslations(locale, ["common"])),
+			...(await serverSideTranslations(locale ?? "ko", ["common"])),
 			// Will be passed to the page component as props
 		},
 	};
-}
+};
